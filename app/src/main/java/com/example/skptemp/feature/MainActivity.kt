343ac8bc18735503f97238dd9fa11e48b1472fbe
kimhm0728 <kimhm0728@naver.com>
2023-12-21 01:21:02 +0900

    feat: 네비게이션 바 메인 액티비티에 적용

--- app/src/main/java/com/example/skptemp/feature/MainActivity.kt
+++ app/src/main/java/com/example/skptemp/feature/MainActivity.kt
@@ -4,13 +4,26 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewTreeObserver
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.TooltipCompat
+import androidx.core.content.ContextCompat
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.databinding.DataBindingUtil
+import androidx.core.view.forEach
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.skptemp.R
+import com.example.skptemp.common.util.ViewUtil.convertPXtoDP
 import com.example.skptemp.databinding.ActivityMainBinding
+import com.example.skptemp.feature.friends.FriendsFragment
+import com.example.skptemp.feature.home.HomeFragment
+import com.example.skptemp.feature.my.MyFragment
+import com.example.skptemp.feature.report.ReportFragment
+import com.google.android.material.shape.CornerFamily
+import com.google.android.material.shape.MaterialShapeDrawable
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -19,11 +32,19 @@ class MainActivity : AppCompatActivity() {
     private var _binding: ActivityMainBinding? = null
     private val binding get() = _binding!!
 
+    private val mFragments = mapOf(
+        R.id.navigation_home to HomeFragment(),
+        R.id.navigation_report to ReportFragment(),
+        R.id.navigation_friends to FriendsFragment(),
+        R.id.navigation_my to MyFragment()
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
 
-        _binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         startSplashScreen()
     }
 
@@ -46,6 +67,22 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         )
+    }
+
+    override fun onResume() {
+        super.onResume()
+        setUpBottomNavigationBar()
+    }
+
+    private fun setUpBottomNavigationBar() = with(binding.bottomNavigationBar) {
+        findNavController(R.id.nav_host_fragment).let { navController ->
+            setupWithNavController(navController)
+        }
+        itemIconTintList = null
+
+        menu.forEach { item ->
+            findViewById<View>(item.itemId).setOnLongClickListener { true }
+        }
     }
 
     override fun onDestroy() {
